5acb92ed30fd9b9eec3be0f2fc76c2cfeb034531
jaykibi <raquibbi@gmail.com>
2020-03-19 20:24:43 -0500

    added list and note components

---
+++ src/components/List.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class List extends React.Component {
+    render() {
+        return (
+            <div className="list-container">
+                list Component
+            </div>
+        );
+    }
+}
+
+export default List;
