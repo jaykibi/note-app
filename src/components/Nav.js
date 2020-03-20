83c86de2f3ffc6cbd2ab4891bdcfc7d6771303b4
jaykibi <raquibbi@gmail.com>
2020-03-19 21:12:57 -0500

    added togglenote to switch state of app, changed logic on nav button to trigger toggle

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 class Nav extends React.Component {
     render() {
+        const { toggleNote, showNote } = this.props
+
         return (
             <div className="nav-container"> 
                 <div className="nav-logo">Kibo</div>
-                <div className="nav-button">
-                    + Note
+                <div className="nav-button" onClick={() => toggleNote()}>
+                    { showNote ? 'cancel' : '+ note' }
                 </div>
             </div>
         );
