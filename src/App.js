8abe1f6f520c122edfc24da0e558fcfa8814518c
jaykibi <raquibbi@gmail.com>
2020-03-28 19:31:11 -0500

    added delete to tags

--- src/App.js
+++ src/App.js
@@ -73,6 +73,12 @@ class App extends React.Component {
     .catch((err) => console.log(err.response.data));
   }
 
+  deleteTag = (noteId, id) => {
+    axios.delete(urlFor(`/tags/${id}`))
+    .then((res) => this.getNote(noteId) )
+    .catch((err) => console.log(err.response.data) );
+  }
+
   render() {
     const { showNote, notes, note, newTag } = this.state;
   
@@ -87,6 +93,7 @@ class App extends React.Component {
             newTag={newTag}
             closeTagForm={this.closeTagForm}
             submitTag={this.submitTag}
+            deleteTag={this.deleteTag}
           /> 
           : 
           <List 
